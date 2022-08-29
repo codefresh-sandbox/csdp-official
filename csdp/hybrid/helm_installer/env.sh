3f101e1a774af5d48deff1dab960423094baa170
Ilia Medvedev <ilia.medvedev@codefresh.io>
2022-08-29 13:21:47 +0300

    try fix app-proxy

---
+++ csdp/hybrid/helm_installer/env.sh
@@ -0,0 +1,9 @@
+export NAMESPACE=helm-poc
+export PLATFORM_URL=https://g.codefresh.io
+export CODEFRESH_USER_TOKEN=630b07b8184cf1fbcca1f6d1.4a7a16f7760d8b40b86157f7bf983733
+export CSDP_RUNTIME_REPO=https://github.com/codefresh-sandbox/csdp-official
+export CSDP_RUNTIME_NAME=helm-installed-runtime-1
+export CSDP_RUNTIME_INGRESS_URL=https://ilia-ingresshost.ngrok.io
+export CSDP_INGRESS_CLASS_NAME=nginx
+export CSDP_INGRESS_CONTROLLER=nginx
+export CODEFRESH_SECRET_NAME=codefresh-token
